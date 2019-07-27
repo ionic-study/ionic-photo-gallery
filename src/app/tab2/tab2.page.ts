cca262bb350a9ddde0b026e10dcc1acf1e11ef5f
zlq4863947 <zlq4863947@gmail.com>
2019-07-27 22:58:43 +0900

    first commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
-import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
+import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
-  selector: "app-tab2",
-  templateUrl: "tab2.page.html",
-  styleUrls: ["tab2.page.scss"]
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
   currentImage: any;
@@ -16,17 +16,17 @@ export class Tab2Page {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
+      mediaType: this.camera.MediaType.PICTURE,
     };
 
     this.camera.getPicture(options).then(
-      imageData => {
-        this.currentImage = "data:image/jpeg;base64," + imageData;
+      (imageData) => {
+        this.currentImage = 'data:image/jpeg;base64,' + imageData;
       },
-      err => {
+      (err) => {
         // Handle error
-        console.log("Camera issue:" + err);
-      }
+        console.log('Camera issue:' + err);
+      },
     );
   }
 }
